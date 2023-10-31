bda1fc66f2957ef2fe8823f33673539ba3ca4f4f
artem_b <budarnykh.a@yandex.ru>
2023-11-01 02:32:29 +0700

    add security middlewares

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 const { mongoose } = require('mongoose');
 const { errors } = require('celebrate');
-
-// TODO validate incoming requests
-// TODO применить все эти замечательные библиотеки
-// const helmet = require('helmet');
-// const escape = require('escape-html');
-// requestsRateLimiter
+const rateLimiter = require('express-rate-limit');
 
 const routes = require('./routes');
+const { RATE_LIMITER_CONFIG } = require('./middlewares/constants');
 
 const {
   unmatchedRouteHandler,
@@ -24,9 +21,11 @@ const app = express();
 
 const { PORT = 3000 } = process.env;
 
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(rateLimiter(RATE_LIMITER_CONFIG));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
---
+++ middlewares/constants.js
@@ -0,0 +1,4 @@
+module.exports.RATE_LIMITER_CONFIG = {
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+};
